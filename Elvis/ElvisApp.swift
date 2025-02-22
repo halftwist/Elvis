9b4de069a7119fde9b5e5ef4378a2b4f79a60c34
John Kearon <jkearon@hotmail.com>
2025-02-22 09:11:01 -0500

    Initial Commit

---
+++ Elvis/ElvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElvisApp.swift
+//  Elvis
+//
+//  Created by John Kearon on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ElvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
